56fed026a7a4dd0c71882da5e6406e9c7c02e4b3
sabler <rob_sable@gap.com>
2020-04-09 13:45:13 -0700

    Forgot to add a dependency

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const bigapp = express();
 const port = 8080;
 
+
+// Static files
+bigapp.use(express.static('public'))
+
+
+// Hello World -- Delete
 bigapp.get ('/', (req, res) => res.send('Hi!'));
 
 
+// Listening...
 bigapp.listen(port, () => console.log('Listening on port 8080! Talk to me at http://localhost:8080'));
