14ccf5d4676a9a091aeff2566159e8a069e76bbb
sabler <rob_sable@gap.com>
2020-04-09 15:15:34 -0700

    save

--- public/js/mk1.js
+++ public/js/mk1.js
@@ -1,1 +1,12 @@
-// As basic as it gets!
+// No frills JS time.
+
+const fetch = require('../../node_modules/node-fetch');
+
+function getDiscountCode() {
+    fetch('http://localhost:8888/requestcode/Sable/123456789')
+        .then(res => res.text())
+        .then(text => console.log(text));
+}
+
+
+getDiscountCode();
