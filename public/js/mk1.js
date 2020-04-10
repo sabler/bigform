90d925a6dc392e911668c0725c5c475e7a8bf023
sabler <rob_sable@gap.com>
2020-04-09 19:23:32 -0700

    It works if Chrome security is turned off

--- public/js/mk1.js
+++ public/js/mk1.js
@@ -2,11 +2,34 @@
 
 const fetch = require('../../node_modules/node-fetch');
 
-function getDiscountCode() {
-    fetch('http://localhost:8888/requestcode/Sable/123456789')
-        .then(res => res.text())
-        .then(text => console.log(text));
+
+function init() {
+    const pocform   = document.getElementById('pocform')
+    pocform.onsubmit = getDiscountCode;
+
+    // Add Event Listener for form submit call
 }
 
+function getDiscountCode() {
+    const lnfield   = document.getElementById('lnfield').value;
+    const idfield   = document.getElementById('idfield').value;
+    const codefield = document.getElementById('codefield');
 
-getDiscountCode();
+console.log(lnfield);
+console.log(idfield);
+    fetch(`http://localhost:8888/requestcode/${lnfield}/${idfield}`)
+        .then(res => res.text())
+        .then(text => {
+            let returnedCode = text;
+            alert(returnedCode);
+        }
+        );
+
+        return false;
+}
+
+function updatePage(code) {
+    console.log(code);
+}
+
+//getDiscountCode('Andre','4563211');
